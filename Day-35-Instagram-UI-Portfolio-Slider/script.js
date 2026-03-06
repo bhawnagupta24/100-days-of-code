0ee0462bbb01b7085de56f3e1f16a19e2db53590
Bhawna Gupta <bhawnasnbgupta77@gmail.com>
2026-03-06 21:47:59 +0530

    Added README for Day 35 Instagram UI Portfolio Slider

---
+++ Day-35-Instagram-UI-Portfolio-Slider/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: false,
+    allowTouchMove: false,
+    navigation: {
+        nextEl: "#rgt-arrow i",
+        prevEl: "#lft-arrow i",
+    },
+});
